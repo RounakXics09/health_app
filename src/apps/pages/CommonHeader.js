69839b89de164816e505de5cad33348cfa01c232
RounakXics09 <rounak@xics.services>
2025-03-17 09:19:10 +0530

    mobile header

--- src/apps/pages/CommonHeader.js
+++ src/apps/pages/CommonHeader.js
@@ -7,6 +7,8 @@ import Button from '@mui/material/Button';
 import Logo from '../assests/logo.svg';
 import { Link, useNavigate } from 'react-router';
 import { motion } from 'framer-motion';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Divider, Drawer, Grid2, IconButton, List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
 
 const pages = [
     { name: 'Home', link: '' },
@@ -16,72 +18,149 @@ const pages = [
     { name: 'Contact', link: 'contact' }
 ];
 
-function CommonHeader() {
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact'];
+
+function CommonHeader(props) {
 
     const navigate = useNavigate();
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
 
     const handleClick = (e) => {
         navigate("/auth/login");
     }
 
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Grid2 sx={{py:2}}>
+            <motion.a
+                href='/'
+                initial={{ opacity: 0, x: -20 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.5 }}
+                className="flex items-center"
+            >
+                <img src={Logo} alt="Logo" />
+            </motion.a>
+            </Grid2>
+            <Divider />
+            <List>
+                {navItems.map((item) => (
+                    <ListItem key={item} disablePadding>
+                        <ListItemButton sx={{ textAlign: 'center' }}>
+                            <ListItemText primary={item} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
     return (
-        <AppBar position="static" className='header'>
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <div className="flex items-center">
-                        <motion.a
-                            href='/'
-                            initial={{ opacity: 0, x: -20 }}
-                            animate={{ opacity: 1, x: 0 }}
-                            transition={{ duration: 0.5 }}
-                            className="flex items-center"
-                        >
-                            <img src={Logo} alt="Logo" />
-                        </motion.a>
-                    </div>
-
-
-                    <Box className="div-center header-menu-section display-sm-none" sx={{ flexGrow: 1 }}>
-                        {pages.map((e, i) => (
-                            <motion.div
-                                key={i}
-                                initial={{ opacity: 0, y: -10 }}
-                                animate={{ opacity: 1, y: 0 }}
-                                transition={{ duration: 0.3, delay: 0.1 * i }}
+        <>
+            <AppBar position="static" className='header'>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <div className="flex items-center display-sm-none">
+                            <motion.a
+                                href='/'
+                                initial={{ opacity: 0, x: -20 }}
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 0.5 }}
+                                className="flex items-center"
                             >
-                                <Button
-                                    key={i}
-                                    className='menu-buttons'
-                                    sx={{ my: 2 }}
-                                >
-                                    <Link to={'/' + e.link} style={{ textDecoration: 'none', color: '#1C163C' }}>{e.name}</Link>
-                                </Button>
-                            </motion.div>
-                        ))}
-                    </Box>
+                                <img src={Logo} alt="Logo" />
+                            </motion.a>
+                        </div>
 
-                    <Box className="header-right-section div-center display-sm-none">
-                        <motion.div
-                            initial={{ opacity: 0, x: 10 }}
-                            animate={{ opacity: 1, x: 0 }}
-                            transition={{ duration: 0.5, delay: 0.6 }}
-                            className='signin'
-                            onClick={() => { handleClick('login') }}
-                        >
-                            Login
-                        </motion.div>
-                        <motion.div
-                            initial={{ opacity: 0, x: 10 }}
-                            animate={{ opacity: 1, x: 0 }}
-                            transition={{ duration: 0.5, delay: 0.7 }}
-                            className='signup div-center'
-                        >
-                            Book a Demo
-                        </motion.div>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar >
+                        <div className="flex items-center display-sm-flex" style={{ display: 'none', justifyContent: 'space-between', width: '100%', alignItems: 'center' }}>
+                            <motion.a
+                                href='/'
+                                initial={{ opacity: 0, x: -20 }}
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 0.5 }}
+                                className="flex items-center"
+                            >
+                                <img src={Logo} alt="Logo" />
+                            </motion.a>
+                            <IconButton
+                                color="#000000"
+                                aria-label="open drawer"
+                                edge="start"
+                                onClick={handleDrawerToggle}
+                                sx={{ mr: 2, display: { sm: 'none' } }}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                        </div>
+
+
+                        <Box className="div-center header-menu-section display-sm-none" sx={{ flexGrow: 1 }}>
+                            {pages.map((e, i) => (
+                                <motion.div
+                                    key={i}
+                                    initial={{ opacity: 0, y: -10 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    transition={{ duration: 0.3, delay: 0.1 * i }}
+                                >
+                                    <Button
+                                        key={i}
+                                        className='menu-buttons'
+                                        sx={{ my: 2 }}
+                                    >
+                                        <Link to={'/' + e.link} style={{ textDecoration: 'none', color: '#1C163C' }}>{e.name}</Link>
+                                    </Button>
+                                </motion.div>
+                            ))}
+                        </Box>
+
+                        <Box className="header-right-section div-center display-sm-none">
+                            <motion.div
+                                initial={{ opacity: 0, x: 10 }}
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 0.5, delay: 0.6 }}
+                                className='signin'
+                                onClick={() => { handleClick('login') }}
+                            >
+                                Login
+                            </motion.div>
+                            <motion.div
+                                initial={{ opacity: 0, x: 10 }}
+                                animate={{ opacity: 1, x: 0 }}
+                                transition={{ duration: 0.5, delay: 0.7 }}
+                                className='signup div-center'
+                            >
+                                Book a Demo
+                            </motion.div>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar >
+            <nav>
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
+        </>
     );
 }
 export default CommonHeader
